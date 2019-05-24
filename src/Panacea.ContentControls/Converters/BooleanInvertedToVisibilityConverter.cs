7c1a6e7a2c12b799230ffb6938398900f3259b7d
Portno <j.remoundos@dotbydot.gr>
2019-05-24 17:31:56 +0300

    fixed xaml errors

---
+++ src/Panacea.ContentControls/Converters/BooleanInvertedToVisibilityConverter.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Controls;
+using System.Windows.Data;
+
+namespace Panacea.ContentControls.Converters
+{
+    class BooleanInvertedToVisibilityConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            return new BooleanToVisibilityConverter().Convert(!(bool)value, targetType, parameter, culture);
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
